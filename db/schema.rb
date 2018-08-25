42db992ceb085e5f573d15684617de3f8e4ab322
DawidGaleziewski <dawidgalsino@vp.pl>
2018-08-25 15:15:35 +0000

    added secure password with bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_073150) do
+ActiveRecord::Schema.define(version: 2018_08_25_150503) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_08_25_073150) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
